92700d088f50d5c6bc5c3e146fa38e1b71711866
Javier Villanueva <javier.villanueva@rookmotion.com>
2021-08-05 18:39:41 -0500

    :art: adding prettier and eslint

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import { FC } from 'react'
 
-const Layout = ({ children }) => {
+const Layout: FC = ({ children }) => {
   return ( 
     <main>
       { children }
@@ -8,4 +8,4 @@ const Layout = ({ children }) => {
   );
 }
  
-export default Layout;
+export default Layout
